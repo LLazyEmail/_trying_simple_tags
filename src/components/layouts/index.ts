8192576185c930a0b826799563f5ac7be30f8628
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2026-09-22 01:48:26 +0300

    1111

--- src/components/layouts/index.ts
+++ src/components/layouts/index.ts
@@ -8,14 +8,3 @@ export {
   imageLayout,
   headingLayout,
 } from './default';
-
-export type {
-  italicLayout,
-  linkLayout,
-  listLayout,
-  listItemLayout,
-  paragraphLayout,
-  strongLayout,
-  imageLayout,
-  headingLayout,
-} from './default';
